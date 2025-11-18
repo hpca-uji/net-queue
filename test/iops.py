0a3a227f21a33bce9b382ff161c09984f7fc0032
Miguel Ángel Prosper <mprosper@uji.es>
2025-11-18 08:21:18 +0100

    unified iops test

--- test/iops.py
+++ test/iops.py
@@ -4,6 +4,7 @@ import math
 import sys
 import time
 import enum
+import random
 from threading import Thread
 from argparse import ArgumentParser, Namespace
 
@@ -29,37 +30,36 @@ class Mode(enum.StrEnum):
 
 # Argument pasrser
 parser = ArgumentParser(prog="nq-test-iops", description="net-queue IOPS test")
-parser.add_argument("proto", choices=list(nq.Protocol))
-parser.add_argument("peer", choices=list(Peer))
-parser.add_argument("mode", choices=list(Mode))
-parser.add_argument("--start-delay", type=float, default=3.0)
-parser.add_argument("--delay", type=float, default=0.0)
-parser.add_argument("--size", type=int, default=2 * 1024 ** 2)
-parser.add_argument("--reps", type=int, default=4 * 1000)
-parser.add_argument("--clients", type=int, default=1)
+parser.add_argument("proto", choices=list(nq.Protocol), help="Which protocol to use")
+parser.add_argument("peer", choices=list(Peer), help="Which peer type to use")
+parser.add_argument("mode", choices=list(Mode), help="Synchronization mode")
+parser.add_argument("--start-delay", type=float, default=3.0, help="Time to wait for server startup")
+parser.add_argument("--delay", type=float, default=0.0, help="Time to wait before recepction start (to cause buffering)")
+parser.add_argument("--min-size", type=int, default=8, help="Exponenet of minimun message size")
+parser.add_argument("--step-size", type=int, default=2, help="Exponenet between message sizes")
+parser.add_argument("--max-size", type=int, default=32, help="Exponenet of maxmimun message size")
+parser.add_argument("--step-expo", type=float, default=0.5, help="Exponenet of number of splits when stepping down a size")
+parser.add_argument("--reps", type=int, default=1, help="Number of repetitions of messages")
+parser.add_argument("--clients", type=int, default=1, help="Number of expected clients for the server")
 
 
-def get(comm: nq.Communicator, msg: numpy.ndarray, reps: int):
+def get(comm: nq.Communicator, msgs: list[bytearray]):
     """Communication get handler"""
-    for i in range(reps):
+    for i in range(len(msgs)):
         print(i, end="\r", flush=True)
-        got = comm.get().data
-        assert len(got) == len(msg), f"Lost message data (got {len(got)}, expect {len(msg)})"
-    assert numpy.array_equal(got, msg), "Corrupted message data"
+        comm.get().data
     print(i)
-    return got
 
 
-def put(comm: nq.Communicator, msg: numpy.ndarray, reps: int):
+def put(comm: nq.Communicator, msgs: list[bytearray]):
     """Communication put handler"""
-    for i in range(reps):
+    for i in range(len(msgs)):
         print(i, end="\r", flush=True)
-        comm.put(msg)
+        comm.put(msgs[i])
     print(i)
-    return msg
 
 
-def convert_size(units: int, scale: int = 1000):
+def convert_size(units: float, scale: int = 1000):
     """Convert unit to use SI suffixes"""
     size_name = ("", "K", "M", "G", "T", "P", "E", "Z", "Y")
     if units > 0:
@@ -72,23 +72,38 @@ def convert_size(units: int, scale: int = 1000):
     return f"{s}{size_name[i]}"
 
 
-def print_stats(config: Namespace, time: float) -> None:
+def print_stats(sizes: list[int], time: float) -> None:
     """Print statistics"""
-    ops = config.reps * 2
-    size = config.size * ops
+    ops = len(sizes)
+    size = sum(sizes)
+    avg = size / ops
     print(f"Time:       {time:.1f}s")
-    print(f"Data:       {convert_size(config.reps)} x {convert_size(config.size)}B")
-    print(f"Transfer:   {convert_size(size)}B @ {convert_size(size * 8 / time):>5}bps")
-    print(f"Operations: {convert_size(ops)} @ {convert_size(ops / time)}IOPS")
+    print(f"Data:       {convert_size(len(sizes))} ~ {convert_size(avg)}B")
+    print(f"Transfer:   {convert_size(size)}B @ {convert_size(size * 8 / time):>5}bps")  # type: ignore
+    print(f"Operations: {convert_size(ops)} @ {convert_size(ops / time)}IOPS")  # type: ignore
+
+
+def generate(config: Namespace) -> list[numpy.ndarray]:
+    """Generate messages"""
+    messages = []
+    buffer = numpy.arange(2 ** config.max_size, dtype=numpy.uint8)
+    for i in range(config.min_size, config.max_size, config.step_size):
+        splits = round(((2 ** config.max_size) / (2 ** i)) ** config.step_expo)
+        for j in range(splits):
+            messages.append(buffer[:2 ** i])
+    messages.append(buffer)
+    messages *= config.reps
+    random.shuffle(messages)
+    return messages
 
 
 def server(config: Namespace):
     """Server peer"""
-    message = numpy.arange(config.size, dtype=numpy.uint8)
+    messages = generate(config)
 
     with nq.new(protocol=config.proto, purpose=nq.Purpose.SERVER) as server:
-        get_thread = Thread(target=get, args=(server, message, config.reps * config.clients))
-        put_thread = Thread(target=put, args=(server, message, config.reps))
+        get_thread = Thread(target=get, args=(server, messages * config.clients))
+        put_thread = Thread(target=put, args=(server, messages))
         for _ in range(config.clients):
             server.get()
         server.put(None)
@@ -107,19 +122,18 @@ def server(config: Namespace):
                 put_thread.join()
 
     end_time = time.time()
-    del message
 
-    print_stats(config=config, time=end_time - start_time)
+    print_stats(sizes=list(map(len, messages)) * config.clients, time=end_time - start_time)
 
 
 def client(config: Namespace):
     """Client peer"""
-    message = numpy.arange(config.size, dtype=numpy.uint8)
+    messages = generate(config)
 
     time.sleep(config.start_delay)
     with nq.new(protocol=config.proto, purpose=nq.Purpose.CLIENT) as client:
-        get_thread = Thread(target=get, args=(client, message, config.reps))
-        put_thread = Thread(target=put, args=(client, message, config.reps))
+        get_thread = Thread(target=get, args=(client, messages))
+        put_thread = Thread(target=put, args=(client, messages))
         client.put(None)
         client.get()
         start_time = time.time()
@@ -137,9 +151,8 @@ def client(config: Namespace):
                 put_thread.join()
 
     end_time = time.time()
-    del message
 
-    print_stats(config=config, time=end_time - start_time)
+    print_stats(sizes=list(map(len, messages)), time=end_time - start_time)
 
 
 def main(config: Namespace):
@@ -147,6 +160,7 @@ def main(config: Namespace):
     self = sys.modules[__name__]
     handler = getattr(self, config.peer)
     print(config)
+    random.seed(0)
     handler(config)
 
 
